ac66d5607f9f2beca7496e69688cb6c76ed7bcf2
Golldiie <Golldiie@users.noreply.github.com>
2026-04-20 12:13:46 +0000

    Deploying to gh-pages from @ Golldiie/Cat-energy-2.0@d7eb4ae8ef4b414514979411682fdeb22e0feeca 🚀

---
+++ 9/scripts/menu-button.js
@@ -0,0 +1,1 @@
+var e=document.querySelector(".page-nav"),n=document.querySelector(".page-nav__toggle");n.onclick=function(){e.classList.toggle("page-nav--opened"),e.classList.toggle("page-nav--closed")};
